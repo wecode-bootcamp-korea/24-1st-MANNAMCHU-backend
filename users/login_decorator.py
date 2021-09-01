0b4ef2566577bd9f799d9a031098afc50e6ce270
hyun-soo choi <hyun-soochoi@hyun-sooui-MacBookAir.local>
2021-09-01 20:08:35 +0900

    users app view and decorator

--- users/login_decorator.py
+++ users/login_decorator.py
@@ -1,23 +1,23 @@
-# import json
-# import jwt
+import json
+import jwt
 
-# from django.http import JsonResponse
+from django.http import JsonResponse
 
-# from my_settings  import SECRET_KEY
-# from users.models import User
+from my_settings  import SECRET_KEY
+from users.models import User
 
-# def Login_decorator(func):
-#     def wrapper(self, request, *args, **kwargs):
-#         try:
-#             access_token = request.headers.get("Authorization", None)
-#             payload      = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
-#             user         = User.objects.get(id=payload['id'])
-#             request.user = user
+def Login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get("Authorization", None)
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
+            user         = User.objects.get(id=payload['id'])
+            request.user = user
 
-#             return func(self, request, *args, **kwargs)
-#         except jwt.exceptions.DecodeError:
-#             return JsonResponse({"message" : "INVALID_TOKEN"}, status=400)
-#         except User.DoesNotExist:
-#             return JsonResponse({"message" : "INVALID_USER"}, status=400)
+            return func(self, request, *args, **kwargs)
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({"message" : "INVALID_TOKEN"}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_USER"}, status=400)
         
-#     return wrapper
+    return wrapper
