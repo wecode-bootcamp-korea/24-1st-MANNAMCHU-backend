0fd11acada495ab7d86a92450c6c2d1ac21ff5e9
hyun-soo choi <hyun-soochoi@hyun-sooui-MacBookAir.local>
2021-09-02 15:20:49 +0900

    users app view and decorator+

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     name     = models.CharField(max_length=45)
     email    = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=40)
+    password = models.CharField(max_length=200)
     phone    = models.CharField(max_length=17)
     address  = models.CharField(max_length=500)
 
