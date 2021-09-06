e11a17c6c52aed3e3a8b8797795374f736ba10f4
hyun-soo choi <hyun-soochoi@hyun-sooui-MacBookAir.local>
2021-09-06 21:09:32 +0900

    products app listview & filter++

--- products/views.py
+++ products/views.py
@@ -7,41 +7,50 @@ from products.models       import Product, Image, ProductTag, Tag, Cart
 
 class ProductListView(View):
     def get(self, request):
+        page       = request.GET.get('page', 1)
+        tag        = request.GET.get('tag')
+        page       = int(page or 1)
+        page_size  = 24
+        limit      = page_size * page 
+        offset     = limit - page_size
+        sale       = Tag.objects.get(name='sale')
         try:
-            page      = request.GET.get('page', 1)
-            tag       = request.GET.get('tag')
-            page      = int(page or 1)
-            page_size = 1
-            limit     = page_size * page 
-            offset    = limit - page_size
-            
-            sale     = Tag.objects.get(name='sale')
-            best     = Tag.objects.get(name='best')
-            new      = Tag.objects.get(name='new')
-
             if not tag:
-                products = Product.objects.all().order_by('-id') [offset:limit]
-        
-            elif str(tag) =='sale':
-                products = Product.objects.filter(tags=sale.id).order_by('-id') [offset:limit]
-            elif str(tag) =='new':
-                products = Product.objects.filter(tags=new.id).order_by('-id') [offset:limit]
-            elif str(tag) =='best':
-                products = Product.objects.filter(tags=best.id).order_by('-id') [offset:limit]
-
+                products   = Product.objects.all().order_by('-id') [offset:limit]
+            else:
+                tag_object = Tag.objects.get(name=tag)
+                products   = Product.objects.filter(tags=tag_object.id).order_by('-id') [offset:limit]
+            
             if not products:
                 return JsonResponse({"message" : "PAGE NOT FOUND"}, status=404)
-                    
+            
             result   = [{
                     'name'       : product.name,
                     'price'      : product.price,
-                    'discount'   : list(ProductTag.objects.filter(product_id=product.id, tag_id=sale.id).values('sale_rate')), # 문제 : 
-                    'image'      : list(Image.objects.filter(product_id=product.id).values('url')),
-                    'tag'        : list(product.tags.values('name')),
+                    'discount'   : [discount.sale_rate for discount in ProductTag.objects.filter(product_id=product.id, tag_id=sale.id)], # 문제 : 
+                    'image'      : [url.url for url in Image.objects.filter(product_id=product.id)],                           #list(Image.objects.filter(product_id=product.id).values('url')),
+                    'tag'        : [tag.name for tag in product.tags.all()],
                     'like_count' : product.like_count
                 } for product in products]
             return JsonResponse({'products': result, 'page' : page }, status=200)
         except Product.DoesNotExist:
             return JsonResponse({"message" : "DATA NOT FOUND"}, status=400)
-        except UnboundLocalError:
+        except Tag.DoesNotExist:
             return JsonResponse({"message" : "TAG DOES NOT EXISTS"}, status=400)
+
+class CartView(View):
+    @login_decorator
+    def get(self, request):
+        user = request.user
+        carts = Cart.objects.filter(user_id=user.id)
+
+        result = [{
+            'product'  : cart.option.product.name,
+            'price' : cart.option.product.price,
+            'option': cart.option.option,
+            'addtional_price' : cart.option.addtional_price,
+            'quantity' : cart.quantity,
+            'image' : cart.option.product.image_set.values('url'),
+            'sale_rate' : cart.option.product.producttag_set.values('sale_rate'),
+        }for cart in carts]
+        return JsonResponse({"message" : result}, status=200)
