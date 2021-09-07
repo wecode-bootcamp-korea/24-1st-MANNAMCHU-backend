da1e965ee23e0ab91e1a4f1222725ff9a0fcb585
Hope Kim <kzang2028@gmail.com>
2021-09-07 16:35:34 +0900

    Added: added cart POST & DELETE methods

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
-from products.views import DetailView
+from products.views import DetailView,CartView
 from django.urls import path
 
 urlpatterns = [
     path('/detail', DetailView.as_view()),
+    path('/cart', CartView.as_view()),
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
+import json
+
 from django.views import View
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.utils import DataError
 
-from products.models import Product, Option, Image, ProductTag, Tag
+from products.models import Product, Option, Image, ProductTag, Tag, Cart
+from users.models import User
 
 
 class DetailView(View):
@@ -37,3 +41,44 @@ class DetailView(View):
         except ObjectDoesNotExist:
             return JsonResponse({'message': 'PRODUCT_NOT_EXIST'}, status=404)
             
+
+class CartView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        try:
+            if not User.objects.filter(id=data['user_id']).exists():
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            if not Option.objects.filter(id=data['option_id']).exists():
+                return JsonResponse({'message': 'INVALID_OPTION'}, status=400)
+
+            if Cart.objects.filter(user_id=data['user_id'], option_id=data['option_id']).exists():
+                option = Cart.objects.get(option_id=data['option_id'])
+                option.quantity += int(data['quantity'])
+                option.save()
+                return JsonResponse({'message': 'ALREADY_IN_YOUR_CART, MORE_ADDED'}, status=201)
+
+            Cart.objects.create(
+                user_id   = data['user_id'],
+                option_id = data['option_id'],
+                quantity  = data['quantity'])
+            return JsonResponse({'message': 'ADDED_ITEM_INTO_YOUR_CART'}, status=201)
+        
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'message': 'VALUE_ERROR'}, status=400)
+        except DataError:
+            return JsonResponse({'message': 'TOO_BIG_NUMBER'}, status=400)
+
+    def delete(self, request):
+        product_id = request.GET.get('option_id')
+
+        try:
+            target_item = Cart.objects.get(option_id=product_id)
+            target_item.delete()
+            return JsonResponse({'message': 'ITEM_DELETED'}, status=200)
+        
+        except ObjectDoesNotExist:
+            return JsonResponse({'message': 'ITEM_DOES_NOT_EXIST'}, status=400)
+
