97a06e59c38bda84675e2ce437492db7dd931a0e
hyun-soo choi <hyun-soochoi@hyun-sooui-MacBookAir.local>
2021-09-07 10:30:31 +0900

    products app listview & filter++

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from products.views import ProductListView, DetailView
 
 urlpatterns = [
-     path('/detail', DetailView.as_view()),
      path('/', ProductListView.as_view()),
+     path('/detail', DetailView.as_view()),
 ]
